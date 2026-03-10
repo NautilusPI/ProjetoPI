8998799e5279c6f79d180d7fd8fa33e02e21016a
eduardoSantana-dev <eduardo.ssantos@sptech.school>
2026-03-10 20:50:47 -0300

    Criação dos codigos do arduino

---
+++ Arduino/temperaturaSimples.ino
@@ -0,0 +1,24 @@
+// Criando variável que recebe o pino onde o sensor está conectado 
+const int PINO_SENSOR_TEMPERATURA = A0;
+// Variável que vai armazenar a temperatura calculada em Celsius
+float temperaturaCelsius;
+// A funçao setup serve para configurar o arduino sem ela o arduino não consegue funciona.
+void setup(){
+    Serial.begin(9600);
+}
+// Função loop roda continuamente enquanto o Arduino estiver ligado, ela funciona em loop
+void loop(){
+     // Lê o valor analógico do sensor no pino A0
+    // O valor retornado vai de 0 a 1023
+    int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
+    // Converte o valor lido para temperatura em Celsius
+    temperaturaCelsius = (valorLeitura * 5.0 /1023.0)/ 0.01;
+    // Exibi a temperatura formatada no console
+    Serial.print("temperatura : ");
+    Serial.print(temperaturaCelsius);
+    // Esse " C" serve para pular uma linha entre uma temperatura e outra
+    Serial.println(" C");
+
+    // Espera 2 segundos para repetir a leitura
+    delay(2000);
+}
