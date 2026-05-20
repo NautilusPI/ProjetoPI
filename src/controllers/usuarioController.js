4e3a82e03ffcc73eaf38c42c170eb1c24b5888b1
liviafaccin <livia.cano@sptech.school>
2026-05-20 20:27:50 -0300

    conectando cadastro de usuarios com o banco de dados

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -55,37 +55,32 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var cpf = req.body.cpfServer;
+    var codigo = req.body.codigoServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+    if (!nome || !email || !cpf || !codigo || !senha) {
+        return res.status(400).send("Preencha todos os campos obrigatórios!");
     }
+
+    usuarioModel.buscarEmpresaPorCodigo(codigo)
+        .then(function (empresas) {
+            if (empresas.length == 0) {
+                return res.status(400).send("Código de ativação inválido!");
+            }
+
+            var idEmpresa = empresas[0].idEmpresa;
+
+            return usuarioModel.cadastrar(nome, email, cpf, senha, idEmpresa)
+                .then(function (resultado) {
+                    res.json({ mensagem: "Cadastro realizado com sucesso!" });
+                });
+        })
+        .catch(function (erro) {
+            console.log("Erro ao cadastrar:", erro);
+            res.status(500).json(erro.sqlMessage);
+        });
 }
 
 module.exports = {
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,26 +3,37 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nome, email, fkEmpresa FROM usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa) {
+function cadastrar(nome, email, cpf, senha, fkEmpresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+        INSERT INTO usuario (Nome, Email, CPF, Senha, fkEmpresa) VALUES ('${nome}', '${email}', '${cpf}', '${senha}', ${fkEmpresa});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
+function buscarEmpresaPorCodigo(codigo) {
+    console.log("Buscando empresa pelo código:", codigo)
+    var instrucaoSql = `
+        SELECT idEmpresa FROM Empresa 
+        WHERE CodigoDeAtivacao = '${codigo}';
+    `;
+    console.log("SQL:", instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar, 
+    buscarEmpresaPorCodigo
 };
